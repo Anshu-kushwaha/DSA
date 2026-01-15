79f9c50b559fee9cb7b0e74dae375a95c6dd0165
Anshu Kushwaha <anshukushwaha112@gmail.com>
2026-01-15 11:30:58 +0530

    Add files via upload

---
+++ ArrayOfOtherDataTypes.java
@@ -0,0 +1,17 @@
+package Arrays;
+
+public class ArrayOfOtherDataTypes {
+    static void main() {
+
+        /*
+        char[] arr = new char[10];
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i] + "  ");
+        } */
+
+        String[] arr = {"Anshu", "Mohit","Suraj"};
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i] + "  ");
+        }
+    }
+}
---
+++ SortArrayBuiltIn.java
@@ -0,0 +1,20 @@
+package Arrays;
+
+import java.util.Arrays;
+
+public class SortArrayBuiltIn {
+    public static void main(String[] args) {
+
+        // sort - ascending order
+        int[] arr = {4, 1, 7, 5, -3, 10, 2};
+        print(arr);          // before sorting
+        Arrays.sort(arr);    // built-in sort
+        print(arr);          // after sorting
+    }
+    static void print(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+}
